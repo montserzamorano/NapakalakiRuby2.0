ee5cd7be75d3e27f7a2eee6125e5f140971b49c3
montse <montse@192.168.1.114>
2016-03-09 01:22:36 +0100

    ee

--- Napakalaki/lib/main.rb
+++ Napakalaki/lib/main.rb
@@ -8,12 +8,31 @@ class EjemploMain
     #Mostrar monstruos que tienen un nivel de combate superior a 10
     Cards.newMonsters
     m = Cards.monstruos
-    m.each do |mons|
-      puts mons.t_s
-    end
     #Tengan un mal rollo que implique solo perdida de niveles
-    #Su mal rollo indique una ganancia de niveles superior a 1
+#    m.each do |mons|
+#      mal = mons.bc
+#      if mal.levels!=0 && mal.nHiddenTreasures==0 && mal.nVisibleTreasures==0 && 
+#          mal.specificHiddenTreasures.empty? && mal.specificVisibleTreasures.empty?
+#          puts mons.t_s
+#      end
+#    end
+
+    #Su buen rollo indique una ganancia de niveles superior a 1
+   # m.each do |mons|
+   #   p = mons.prize
+   #   if p.levels>1
+   #       puts mons.t_s
+   #   end
+   # end
     #Su mal rollo suponga la perdida de un determinado tipo de tesoros
+    m.each do |mons|
+      mal = mons.bc
+      (mal.specificVisibleTreasures).each do |s|
+        if s == TreasureKind::ARMOR
+          puts mons.t_s
+        end
+      end
+    end
   end
 end
 if $0 == __FILE__
